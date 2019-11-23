a10327f27584e5895de6d2e51574ca3ddbbca5e6
xiana <1183999239@qq.com>
2019-11-23 17:55:16 +0800

    gl  11.23  第1次更新

--- house-producer-8081/src/main/java/com/shana/house/service/IFrontHouseService.java
+++ house-producer-8081/src/main/java/com/shana/house/service/IFrontHouseService.java
@@ -3,5 +3,5 @@ package com.shana.house.service;
 import com.shana.house.model.House;
 
 public interface IFrontHouseService {
-    void addHouseNameAndDes(House house);
+    void addHouseNameAndDes(House house, String[] imgs1);
 }
